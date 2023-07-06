64c9577e3349feb0244c8f28d4714aab61439efe
Oliver <20215296@s.ubaguio.edu>
2023-07-06 10:20:21 +0800

    navbar done

--- finalproject_g1/src/Components/Navbar.js
+++ finalproject_g1/src/Components/Navbar.js
@@ -14,8 +14,8 @@ function Navbar() {
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon fs-4"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0 text-center">
+                <div class="collapse navbar-collapse " id="navbarNav">
+                    <ul class="navbar-nav ms-auto mb-3 mb-lg-0">
 
                         <li class="nav-item">
                             <Link to={`/references`} className="nav-link text-danger fw-medium fs-6">References</Link>
