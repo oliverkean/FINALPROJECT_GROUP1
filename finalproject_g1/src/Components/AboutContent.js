5f40d306ba9981100d655779af6743d933e57324
Oliver <20215296@s.ubaguio.edu>
2023-07-10 10:31:32 +0800

    aboutss

---
+++ finalproject_g1/src/Components/AboutContent.js
@@ -0,0 +1,21 @@
+function AboutContent({image, title, text}) {
+    return(
+        <div className="col mt-5 mx-3">
+            <div className="row d-flex justify-content-center align-items-center">
+                <div className="col col-lg-6 col-md-12">
+                    <img style={{ width: '40vw', height: '100%' }} src={image} alt="icon" className="rounded"/>
+                </div>
+                <div className="col col-lg-6 col-md-12">
+                    <div className="my-5">
+                        <h1 className="fw-bold text-white">{title}</h1>
+                    </div>
+                    <div className="my-3">
+                        <span className="fs-5 text-secondary text-white">{text}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AboutContent
--- finalproject_g1/src/Pages/AboutPage.js
+++ finalproject_g1/src/Pages/AboutPage.js
@@ -1,31 +1,25 @@
+import AboutContent from "../Components/AboutContent";
+import Footer from "../Components/Footer";
 import Heading from "../Components/Heading";
 import Navbar from "../Components/Navbar";
+import TwoColGrid from "../Layouts/TwoColGrid";
+import img1 from '../Assets/images/pexels-maël-balland-13713058.jpg'
 
 function About() {
     return(
         <>
             <Navbar/>
 
-            <div className="container-fluid d-flex justify-content-center align-items-center" style={{ height: '50vh', backgroundPosition:'center', overflow:'hidden', backgroundSize: 'cover', backgroundImage: `url("https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80")`}}>
-                <Heading 
-                    title="ABOUT US"
-                    text=""
-                    size="1"
-                    weight="bold"
-                    color="white"
-                    align="center"
-                />
+            <div className="container-fluid d-flex justify-content-center align-items-center" style={{ height: '100vh', backgroundPosition:'center', overflow:'hidden', backgroundSize: 'cover', backgroundImage: `linear-gradient(to left, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.8)), url("https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80")` }}>
+                <TwoColGrid>
+                    <AboutContent
+                        title="About"
+                        image = {img1}
+                        text="The terms equity and equality are often used interchangeably in education, but it is important to understand the difference between them. While both have their benefits, equity should be the ultimate goal for educators when addressing issues faced by disadvantaged students."
+                    />
+                </TwoColGrid>
             </div>
-
-            <div className="container">
-                <Heading 
-                    text=""
-                    size="1"
-                    weight="bold"
-                    color="black"
-                    align="center"
-                />
-            </div>
+            <Footer/>
             
 
         </>
