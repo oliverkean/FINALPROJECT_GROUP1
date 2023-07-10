a7a3e954fa0bb0d9044f149e198ef2808266a08e
Oliver <20215296@s.ubaguio.edu>
2023-07-10 10:41:14 +0800

    about di pa tapos

--- finalproject_g1/src/Components/AboutContent.js
+++ finalproject_g1/src/Components/AboutContent.js
@@ -2,16 +2,12 @@ function AboutContent({image, title, text}) {
     return(
         <div className="col mt-5">
             <div className="row d-flex justify-content-center align-items-center">
-                <div className="col col-lg-6 col-md-12">
-                    <img style={{ width: '40vw', height: '100%' }} src={image} alt="icon" className="rounded"/>
+                <div className="col-4 col-md-12">
+                    <img style={{ width: '30vw', height: '100%' }} src={image} alt="icon" className="rounded"/>
                 </div>
-                <div className="col col-lg-6 col-md-12">
-                    <div className="my-5">
-                        <h1 className="fw-bold text-white">{title}</h1>
-                    </div>
-                    <div className="my-3">
-                        <span className="fs-5 text-secondary text-white">{text}</span>
-                    </div>
+                <div className="col-4 col-md-12">
+                    <h1 className="fw-bold text-white">{title}</h1>
+                    <span className="fs-5 text-secondary text-white">{text}</span>
                 </div>
             </div>
         </div>
