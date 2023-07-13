0ecffa8b550df19e5437d3cab6ee3d31e6f658f9
oliverkean <20215296@s.ubaguio.edu>
2023-07-14 01:20:16 +0800

    done

--- finalproject_g1/src/Components/AboutContent.js
+++ finalproject_g1/src/Components/AboutContent.js
@@ -1,10 +1,10 @@
-function AboutContent({title, text, text_color, title_color}) {
+function AboutContent({title, text, text_color, title_color, text_size}) {
     return(
         <div className="col">
             <div className="row d-flex justify-content-center align-items-center mx-4">
                 <div className="col-12 mb-5 text-center">
                     <h1 className={`fw-bold text-danger text-${title_color} mt-5 mb-3`}>{title}</h1>
-                    <p className={`text-secondary text-${text_color} text-break`}>{text}</p>
+                    <p className={`text-secondary fs-${text_size} text-${text_color} text-break`}>{text}</p>
                 </div>
             </div>
         </div>
--- finalproject_g1/src/Pages/AboutPage.js
+++ finalproject_g1/src/Pages/AboutPage.js
@@ -10,47 +10,42 @@ function About() {
     return(
         <>
             <Navbar/>
-
-            <div className="container-fluid d-flex justify-content-center align-items-center flex-column" style={{height:'50vh'}}>
-                <img width='80' height='80' src="https://marketplace.canva.cn/8cjhM/MAEpYJ8cjhM/1/tl/canva-MAEpYJ8cjhM.png" alt="icon" className="mb-3"/>
-                <h1 className="fw-bold text-danger fs-1">ABOUT</h1>
-            </div>
-
-            <div className="container-fluid d-flex justify-content-center align-items-center" style={{ height: '100vh', backgroundPosition:'center', overflow:'hidden', backgroundSize: 'cover', backgroundImage: `linear-gradient(to top, rgba(0,0,0,1) 0%, rgba(0,0,0,0.7) 50%, rgba(78,77,77,1)),url("https://images.pexels.com/photos/716281/pexels-photo-716281.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")`}}>                
+            
+            <div className="container-fluid d-flex justify-content-center align-items-center" style={{ height: '60vh', backgroundPosition:'center', overflow:'hidden', backgroundSize: 'cover', backgroundImage: `linear-gradient(to top, rgba(0,0,0,1) 0%, rgba(0,0,0,0.7) 50%, rgba(78,77,77,1)),url("https://images.pexels.com/photos/716281/pexels-photo-716281.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")`}}>                
                 <div className="container">
                     <AboutContent
-                        title="MISSION"
-                        text="To promote equitable access to knowledge and skills for all individuals, regardless of their background or circumstances. We strive to create inclusive learning environments where individuals can develop not only academic proficiency but also essential life skills, critical thinking abilities, and a strong sense of social responsibility. Through our collective efforts, we aim to empower learners, foster curiosity, and inspire a lifelong love for learning."
+                        title="GOAL 4"
+                        text="Ensure inclusive and equitable quality education and promote lifelong learning opportunities for all"
                         text_color={'white'}
                         title_color={'danger'}
+                        text_size={3}
                     />
                 </div>
             </div>
 
-            {/* <div className="container">
+            <div className="container-fluid mb-5">
                 
-                <AboutContent
-                        title="TARGETS AND INDICTORS"
-                        text=""
-                        text_color={'dark'}
-                        title_color={'dark'}
-                    />
+                <div className="container">
+                    <AboutContent
+                            title="Sustainable Development Goal 4"
+                            text_color={'dark'}
+                            title_color={'dark'}
+                        />
 
-                <BodyContent2 
-                    img={img1}
-                    title="Title"
-                    text="Lorem impsum dolor sit amet."
-                    ordL={0}
-                    ordR={0}
-                />
-                <BodyContent2 
-                    img={img1}
-                    title="Title"
-                    text="Lorem impsum dolor sit amet."
-                    ordL={1}
-                    ordR={0}
-                />
-            </div> */}
+                    <div className="row">
+                        <div className="col-12 col-md-6">
+                            <img src={img1} alt="Image" className="img-fluid shadow-lg rounded"/>
+                        </div>
+
+                        <div className="col-12 col-md-6">
+                            <div>
+                                <p className="fs-5 text-secondary">Aims at ensuring inclusive and equitable quality education and promote lifelong learning opportunities for all. This goal ensures that all girls and boys complete free primary and secondary schooling by 2030. This goal ensures that all girls and boys complete free primary and secondary schooling by 2030. It also aims to provide equal access to affordable vocational training, to eliminate gender and wealth disparities and achieve universal access to a quality higher education.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
             <Footer/>
             
 
