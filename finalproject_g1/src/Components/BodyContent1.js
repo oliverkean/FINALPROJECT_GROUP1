b28022f21e6ab7c7e19a8c6cec78d0468f00bc94
glennbaday <20208860@s.ubaguio.edu>
2023-07-14 06:55:53 +0800

    fixed responsiveness of body content 1 and 3

--- finalproject_g1/src/Components/BodyContent1.js
+++ finalproject_g1/src/Components/BodyContent1.js
@@ -13,19 +13,19 @@ function BodyContent1({content}) {
     return(
        <>
         {content.map((content, index) => (
-            <div className="col-12 col-sm-12 col-md-12 col-lg-4 text-sm-start" data-aos="fade-up">
+            <div className="col-12 col-sm-12 col-md-12 col-lg-4 text-md-start" data-aos="fade-up">
                 <div className="row align-items-lg-center justify-content-center">
-                    <div className="col-sm-6 col-md-6 col-lg-12 my-md-0 my-sm-3 my-3" data-aos="fade-right">
-                        <div className="row align-items-center align-items-md-start w-100">
-                            <div className="col-3 col-lg-3 col-sm-12 my-lg-3">
+                    <div className="col-sm-5 col-md-6 col-lg-12 my-md-3 my-sm-3 my-3" data-aos="fade-right">
+                        <div className="row align-items-center text-center text-md-start text-sm-left align-items-md-start w-100">
+                            <div className="col-12 col-md-3 col-lg-3 col-sm-12 my-lg-3 my-1">
                                 <img className="img-fluid" src={content.image} alt="icon"/>
                             </div>
-                            <div className="col-9 col-lg-9 col-sm-12 my-lg-3">
+                            <div className="col-12 col-md-9 col-lg-9 col-sm-12 my-lg-3">
                                 <span className="fs-5 fw-bold">{content.title}</span>
                             </div>
                         </div>
                     </div>
-                    <div className="col-sm-6 col-md-6 col-lg-12 my-md-0 my-sm-3 my-3" data-aos="fade-left">
+                    <div className="col-sm-7 col-md-6 col-lg-12 my-md-3 my-sm-3 my-3 text-center text-sm-start" data-aos="fade-left">
                         <span className="fs-6 text-white">{content.text}</span>
                     </div>
                 </div>
--- finalproject_g1/src/Components/BodyContent3.js
+++ finalproject_g1/src/Components/BodyContent3.js
@@ -13,14 +13,14 @@ function BodyContent3({image, title, text}) {
       }, [])
     return(
         <div className="col-sm-12 col-md-12 col-lg-6 my-3" data-aos="fade-up">
-            <div className="row align-items-md-center text-lg-center px-lg-5 px-sm-0">
-                <div className="col-sm-4 col-md-4 col-lg-12 text-sm-center my-1 my-lg-0">
+            <div className="row align-items-center align-items-md-center text-lg-center px-lg-5 px-sm-0">
+                <div className="col-3 col-sm-3 col-md-3 col-lg-12 text-sm-center my-1 my-lg-0 text-center">
                     <img className="img-fluid" style={{ width: '64px', height: '64px' }} src={image} alt="icon"/>
                 </div>
-                <div className="col-sm-4 col-md-4 col-lg-12 my-1 my-lg-0">
+                <div className="col-9 col-sm-3 col-md-3 col-lg-12 my-1 my-lg-0">
                     <span className="fs-5 fw-bold">{title}</span>
                 </div>
-                <div className="col-sm-4 col-md-4 col-lg-12 my-1 my-lg-0">
+                <div className="col-12 col-sm-6 col-md-6 col-lg-12 my-1 my-lg-0">
                     <span className="fs-6 text-secondary">{text}</span>
                 </div>
             </div>
