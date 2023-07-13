ada8b08e6abc4c479984ded1fe1e603bb36c1ff1
oliverkean <20215296@s.ubaguio.edu>
2023-07-13 22:35:02 +0800

    mission

--- finalproject_g1/src/Components/AboutContent.js
+++ finalproject_g1/src/Components/AboutContent.js
@@ -4,7 +4,7 @@ function AboutContent({title, text, text_color}) {
             <div className="row d-flex justify-content-center align-items-center mx-4">
                 <div className="col-12 mb-5 text-center">
                     <h1 className="fw-bold text-danger mb-3">{title}</h1>
-                    <p className={`text-secondary text-${text_color} text-break`}>{text}</p>
+                    <p className={`text-secondary text-${text_color}`}>{text}</p>
                 </div>
             </div>
         </div>
