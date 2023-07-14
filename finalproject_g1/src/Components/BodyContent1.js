478689097f1cd02af3ff739d9bc1a423c9c0db47
glennbaday <20208860@s.ubaguio.edu>
2023-07-14 09:29:24 +0800

    asdasdasdasdasdasd

--- finalproject_g1/src/Components/BodyContent1.js
+++ finalproject_g1/src/Components/BodyContent1.js
@@ -1,5 +1,6 @@
 function BodyContent1({image, title, text}) {
     return(
+<<<<<<<<< Temporary merge branch 1
 <<<<<<< HEAD
         <>
             <div className="row align-items-center justify-content-center border">
@@ -23,6 +24,29 @@ function BodyContent1({image, title, text}) {
                 </div>
             </div>
         </>
+=========
+       <>
+        {content.map((content, index) => (
+            <div className="col-12 col-sm-12 col-md-12 col-lg-4 text-md-start" data-aos="fade-up">
+                <div className="row align-items-lg-center justify-content-center">
+                    <div className="col-sm-5 col-md-6 col-lg-12 my-md-3 my-sm-3 my-3" data-aos="fade-right">
+                        <div className="row align-items-center text-center text-md-start text-sm-left align-items-md-start w-100">
+                            <div className="col-12 col-md-3 col-lg-3 col-sm-12 my-lg-3 my-1">
+                                <img className="img-fluid" src={content.image} alt="icon"/>
+                            </div>
+                            <div className="col-12 col-md-9 col-lg-9 col-sm-12 my-lg-3">
+                                <span className="fs-5 fw-bold text-danger">{content.title}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-7 col-md-6 col-lg-12 my-md-3 my-sm-3 my-3 text-center text-sm-start" data-aos="fade-left">
+                        <span className="fs-6 text-white fw-semibold">{content.text}</span>
+                    </div>
+                </div>
+            </div>
+        ))}
+       </>
+>>>>>>>>> Temporary merge branch 2
     );
 }
 
